1290b352987c31801f21f67a894879a3a7b1bdf2
Davide Pedron <davide@happydev.cloud>
2020-08-18 19:37:51 +0200

    Print file list

--- main-ui/src/main/java/cloud/happydev/music/ui/App.java
+++ main-ui/src/main/java/cloud/happydev/music/ui/App.java
@@ -5,9 +5,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class App extends Application {
 
@@ -16,12 +22,63 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        songRepository = new SongRepository();
-        songRepository.findAll().forEach(System.out::println);
+        stage.setTitle("HappyMusic App");
 
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        Button button = new Button("Select Directory");
+
+        TextArea textArea = new TextArea();
+        System.setOut(new PrintStream(System.out) {
+            @Override
+            public void write(byte[] buf, int off, int len) {
+                super.write(buf, off, len);
+                String msg = new String(buf, off, len);
+                textArea.setText(textArea.getText() + msg);
+            }
+        });
+
+        VBox vBox = new VBox(button, textArea);
+        Scene scene = new Scene(vBox, 960, 600);
+
         stage.setScene(scene);
         stage.show();
+
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        button.setOnAction(e -> {
+            textArea.setText("");
+            File selectedDirectory = directoryChooser.showDialog(stage);
+
+            if (selectedDirectory != null) {
+                textArea.setText(textArea.getText() + "List file in: " + selectedDirectory.getAbsolutePath());
+                listDirectory(selectedDirectory, 0, textArea);
+            } else {
+                textArea.setText(textArea.getText() + "No directory chosen.");
+            }
+        });
+    }
+
+    public void listDirectory(File dir, int level, TextArea textArea) {
+        File[] firstLevelFiles = dir.listFiles((file, name) ->
+                !file.isHidden()
+                && !(name.startsWith(".") || name.startsWith("_"))
+                && (file.isDirectory() || name.toLowerCase().endsWith(".mp3"))
+        );
+        if (firstLevelFiles != null && firstLevelFiles.length > 0) {
+            for (File aFile : firstLevelFiles) {
+                for (int i = 0; i < level; i++) {
+                    textArea.setText(textArea.getText() + "\t");
+                }
+                if (aFile.isDirectory()) {
+                    textArea.setText(textArea.getText() + "[" + aFile.getName() + "]\n");
+                    if (level < 5) {
+                        listDirectory(aFile, level + 1, textArea);
+                    }
+                } else {
+                    textArea.setText(textArea.getText() + aFile.getName() + "\n");
+                }
+            }
+        }
     }
 
     static void setRoot(String fxml) throws IOException {
